320a6a7dec50ed8774ebfd9ee02bda2cbd4743e5
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-05-24 18:16:02 +0530

    feat : Home Component layout

---
+++ src/Components/Pages/Cities/Cities.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { BsFillHouseFill } from "react-icons/bs";
+import { FaCity } from "react-icons/fa";
+import "../Home/Home.css";
+
+const Cities = () => {
+  return (
+    <div className="parent-container">
+      <div className="container">
+        <div className="side-menu">
+          <div className="side-menu__item">
+            <NavLink
+              exact
+              to="/"
+              className="side-menu__link"
+              activeClassName="active-link"
+            >
+              <BsFillHouseFill className="side-menu__icon" /> Home
+            </NavLink>
+          </div>
+          <div className="side-menu__item">
+            <NavLink
+              to="/cities"
+              className="side-menu__link"
+              activeClassName="active-link"
+            >
+              <FaCity className="side-menu__icon" /> Cities
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cities;
---
+++ src/Components/Pages/Home/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { BsFillHouseFill } from "react-icons/bs";
+import { FaCity } from "react-icons/fa";
+import "../Home/Home.css";
+import Favorites from "../../Favorites/Favorites";
+
+const Home = () => {
+  return (
+    <div className="parent-container">
+      <div className="container">
+        <div className="side-menu">
+          <div className="side-menu__item">
+            <NavLink
+              exact
+              to="/"
+              className="side-menu__link"
+              activeClassName="active-link"
+            >
+              <BsFillHouseFill className="side-menu__icon" /> Home
+            </NavLink>
+          </div>
+          <div className="side-menu__item">
+            <NavLink
+              to="/cities"
+              className="side-menu__link"
+              activeClassName="active-link"
+            >
+              <FaCity className="side-menu__icon" /> Cities
+            </NavLink>
+          </div>
+        </div>
+        {/* Favorites component goes here */}
+        <Favorites />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
