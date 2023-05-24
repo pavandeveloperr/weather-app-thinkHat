8dcab4ce22837c73e9efeab19ca39df5daf98b0d
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-05-24 23:09:53 +0530

    feat : add cities modal

--- src/Components/Pages/Cities/CityNames/CityNames.js
+++ src/Components/Pages/Cities/CityNames/CityNames.js
@@ -1,9 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "../CityNames/CityNames.css";
 import { RiAddBoxFill } from "react-icons/ri";
 import { AiOutlineStar } from "react-icons/ai";
 
+const Modal = ({ onClose }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const cities = [
+    "Mumbai",
+    "Delhi",
+    "Gujarat",
+    "Chennai",
+    "Bengaluru",
+    "Mysore",
+    "Hubli",
+    "Darwad",
+    "Raichur",
+  ];
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredCities = cities.filter((city) =>
+    city.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-header">
+          <p>Cities</p>
+          <div className="modal-footer">
+            <button onClick={onClose}>Close</button>
+          </div>
+        </div>
+        <div className="modal-body">
+          <input
+            type="text"
+            placeholder="Search city"
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+          <ul className="city-list">
+            {filteredCities.map((city) => (
+              <>
+                <li key={city}>
+                  {city}
+                  <span>
+                    <RiAddBoxFill className="Add-city-btn" />
+                  </span>
+                </li>
+              </>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const CityNames = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleAddCityClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="main-container">
       <div className="cities-container">
@@ -11,7 +77,10 @@ const CityNames = () => {
           <div className="city-title">
             <p>Cities</p>
             <span>
-              <RiAddBoxFill className="Add-city-btn" />
+              <RiAddBoxFill
+                className="Add-city-btn"
+                onClick={handleAddCityClick}
+              />
             </span>
           </div>
           <div className="city-card">
@@ -45,6 +114,7 @@ const CityNames = () => {
           </div>
         </div>
       </div>
+      {isModalOpen && <Modal onClose={handleCloseModal} />}
     </div>
   );
 };
